16ce8ecac1b98c6aff2ef9dc89bdd8c5acff9435
Patrick Elsen <pelsen@xfbs.net>
2019-05-15 20:20:04 +0200

    adds some comments

--- src/bin/afp.rs
+++ src/bin/afp.rs
@@ -12,30 +12,31 @@ fn build_ui(app: &gtk::Application) {
     let window = gtk::ApplicationWindow::new(app);
     window.set_title("Amateurfunkprüfung");
 
-    let datastore = DataStore::load("");
+    // load notebook view and overview tab
     let area = gtk::Notebook::new();
     let button = gtk::Button::new_with_label("Übersicht");
     let label = gtk::Label::new("Übersicht");
     area.append_page(&button, Some(&label));
 
+    // load other tabs from data store
+    let datastore = DataStore::load("");
     for section in datastore.sections() {
         let button = gtk::Button::new_with_label(section.short());
         let label = gtk::Label::new(section.short());
         area.append_page(&button, Some(&label));
     }
 
-    let button = gtk::Button::new_with_label("Betrieb & Vorschriften");
-    let label = gtk::Label::new("Betrieb & Vorschriften");
-    area.append_page(&button, Some(&label));
-
+    // position window and make visible
     window.add(&area);
     window.set_default_size(500, 400);
     window.set_position(gtk::WindowPosition::Center);
     window.show_all();
 
+    /*
     button.connect_clicked(|_| {
         println!("Clicked!");
     });
+    */
 }
 
 fn main() {
