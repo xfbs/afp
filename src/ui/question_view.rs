833d057803f16a082b5d47e94f4db2d69f10d3e9
Patrick Elsen <pelsen@xfbs.net>
2019-05-18 18:30:43 +0200

    trying to get stuff to work

--- src/ui/question_view.rs
+++ src/ui/question_view.rs
@@ -1,9 +1,11 @@
 extern crate gtk;
+extern crate glib;
 
 use std::cell::RefCell;
 use std::rc::Rc;
 use gtk::prelude::*;
 use crate::*;
+use glib::signal::SignalHandlerId;
 
 #[derive(Debug, Clone)]
 pub struct QuestionView {
@@ -14,8 +16,9 @@ pub struct QuestionView {
     subsection: gtk::Label,
     id: gtk::Label,
     question: gtk::Label,
+    answers: gtk::Grid,
     choose: Rc<RefCell<Vec<gtk::Button>>>,
-    answer: gtk::Label,
+    answer: Rc<RefCell<Vec<gtk::Label>>>,
     back: gtk::Button,
 }
 
@@ -28,9 +31,10 @@ impl QuestionView {
             section: gtk::Label::new(None),
             subsection: gtk::Label::new(None),
             id: gtk::Label::new(None),
+            answers: gtk::Grid::new(),
             question: gtk::Label::new(None),
             choose: Rc::new(RefCell::new(Vec::new())),
-            answer: gtk::Label::new(None),
+            answer: Rc::new(RefCell::new(Vec::new())),
             back: gtk::Button::new_from_icon_name("go-previous", gtk::IconSize::Button),
         }
     }
@@ -43,6 +47,9 @@ impl QuestionView {
         self.body.set_margin_end(10);
         self.body.set_column_spacing(20);
         self.body.set_row_spacing(20);
+        self.answers.set_column_homogeneous(true);
+        self.answers.set_column_spacing(10);
+        self.answers.set_row_spacing(5);
         self.title_box.add(&self.back);
         self.title_box.set_center_widget(&self.title);
         self.body.attach(&self.title_box, 0, 0, 4, 1);
@@ -50,7 +57,7 @@ impl QuestionView {
         self.body.attach(&self.subsection, 1, 2, 2, 1);
         self.body.attach(&self.id, 1, 3, 1, 1);
         self.body.attach(&self.question, 2, 3, 1, 1);
-        self.body.attach(&self.answer, 2, 4, 1, 1);
+        self.body.attach(&self.answers, 1, 4, 2, 1);
         self.section.set_hexpand(true);
         self.title.get_style_context().add_class("title");
         self.section.get_style_context().add_class("subtitle");
@@ -62,19 +69,30 @@ impl QuestionView {
         self.subsection.set_text(question.subsubsection());
         self.id.set_text(question.id());
         self.question.set_text(question.question());
-        self.answer.set_text(&question.answers()[0]);
 
         // setup answer button
         for (i, answer) in question.answers().iter().enumerate() {
             if self.choose.borrow().get(i).is_none() {
                 let button = gtk::Button::new();
-                self.body.attach(&button, 1, 4 + i as i32, 1, 1);
+                self.answers.attach(&button, 0, i as i32, 1, 1);
                 self.choose.borrow_mut().push(button);
             }
 
             if let Some(button) = self.choose.borrow().get(i) {
                 button.set_label(&format!("{}", i + 1));
                 button.show();
+            }
+
+            if self.answer.borrow().get(i).is_none() {
+                let label = gtk::Label::new(None);
+                self.answers.attach(&label, 1, i as i32, 4, 1);
+                label.set_hexpand(false);
+                self.answer.borrow_mut().push(label);
+            }
+
+            if let Some(label) = self.answer.borrow().get(i) {
+                label.set_text(&answer);
+                label.show();
             }
         }
     }
